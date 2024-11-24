fbbd657c2b49a4e423ea487a6d5736ce9573d402
Almazz <050606500373@stud.satbayev.university>
2024-11-24 19:16:42 +0500

    update

--- javascript/main.js
+++ javascript/main.js
@@ -7,3 +7,26 @@
 //     document.getElementById("header").style.height = "65px";
 //   }
 // }
+
+const banner = document.querySelector('.banner-2');
+const slides = banner.querySelectorAll('div');
+let currentIndex = 0;
+
+function scrollToNextSlide() {
+    currentIndex++;
+    if (currentIndex >= slides.length) {
+        currentIndex = 0;
+        banner.scrollTo({
+            left: 0,
+            behavior: 'smooth'
+        });
+    } else {
+        banner.scrollTo({
+            left: slides[currentIndex].offsetLeft,
+            behavior: 'smooth'
+        });
+    }
+}
+
+setInterval(scrollToNextSlide, 2000);
+
